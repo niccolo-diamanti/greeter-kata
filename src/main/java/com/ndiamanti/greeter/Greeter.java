161acf9386596c0da9d849e4f410cb2f170130f5
Niccolò Diamanti <niccolo.diamanti@gmail.com>
2021-02-18 12:35:38 +0100

    greet method check time and return good morning if in range
    3. greet returns Good morning <name> when the time is 06:00-12:00

--- src/main/java/com/ndiamanti/greeter/Greeter.java
+++ src/main/java/com/ndiamanti/greeter/Greeter.java
@@ -1,10 +1,43 @@
 package com.ndiamanti.greeter;
 
+import java.time.LocalTime;
+
 public class Greeter {
 
+    private LocalTime time;
+
+    public Greeter() {
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
     public String greet(String name) {
-        name = name.trim();
-        name = name.substring(0, 1).toUpperCase() + name.substring(1);
-        return "Hello " + name;
+        if (name != null)
+            name = trimAndCapitalizeFirstLetter(name);
+        else
+            throw new NullNameException();
+
+        if (isTimeInRange(LocalTime.of(6, 0), LocalTime.of(12, 0)))
+            return "Good morning " + name;
+        else
+            return "Hello " + name;
+    }
+
+    private boolean isTimeInRange(LocalTime min, LocalTime max) {
+        return time != null && time.isAfter(min) && time.isBefore(max);
+    }
+
+    private String trimAndCapitalizeFirstLetter(String str) {
+        str = str.trim();
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+
+    public static class NullNameException extends RuntimeException {
+
+        public NullNameException() {
+            super("Name parameter cannot be null");
+        }
     }
 }
